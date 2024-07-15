f084e9ba654f1b82166d254d7923ba371ccd8094
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-07-15 15:07:07 +0930

    Update html, js, css files

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,90 +7,92 @@ const currentCity = document.getElementById('current-city');
 const currentTemp = document.getElementById('current-temp');
 const currentWind = document.getElementById('current-wind');
 const currentHumidity = document.getElementById('current-humidity');
+const forecastHeader = document.getElementById('forecast-header');
 const forecastContainer = document.getElementById('forecast');
 
 let searchHistoryList = JSON.parse(localStorage.getItem('search-history-list')) || [];
 
 function searchCity(event) {
-    event.preventDefault();
-    
-    const city = cityInput.value.trim();
+  event.preventDefault();
+  
+  const city = cityInput.value.trim();
 
-    if (city && !searchHistoryList.includes(city)) {
-      fetchWeather(city);
-      saveToLocalStorage(city);
-      renderSearchHistory();
-    } else if (city) {
-      fetchWeather(city);
-    }
+  if (city && !searchHistoryList.includes(city)) {
+    fetchWeather(city);
+    saveToLocalStorage(city);
+    renderSearchHistory();
+  } else if (city) {
+    fetchWeather(city);
+  }
 }
 
 function fetchWeather(city) {
-    const APIKey = "679aec2d75b9281a253c0028e5cf4db8";
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-    
-    fetch(queryURL)
-        .then(function (response) {
-          if (!response.ok) {
-            throw response.json();
-          }
-    
-          return response.json();
-        })
+  const APIKey = "679aec2d75b9281a253c0028e5cf4db8";
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
   
-        .then(function (weatherData) {
-            const lat = weatherData.coord.lat;
-            const lon = weatherData.coord.lon;
+  fetch(queryURL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+  
+    .then(function (weatherData) {
+      const lat = weatherData.coord.lat;
+      const lon = weatherData.coord.lon;
 
-            fetchForcast(lat, lon);
-            displayWeather(weatherData);
-        })
+      fetchForcast(lat, lon);
+      displayWeather(weatherData);
+      })
         
-        .catch(function (error) {
-          console.error(error);
-          alert(`No weather data found for ${city}!`);
-        });
+    .catch(function (error) {
+      console.error(error);
+      alert(`No weather data found for ${city}!`);
+    });
 }
 
 function fetchForcast(lat, lon) {
-        const APIKey = "679aec2d75b9281a253c0028e5cf4db8";
-        let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+  const APIKey = "679aec2d75b9281a253c0028e5cf4db8";
+  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
-        fetch(forecastURL)
-        .then(function (response) {
-          if (!response.ok) {
-            throw response.json();
-          }
-    
-          return response.json();
-        })
+  fetch(forecastURL)
+  .then(function (response) {
+    if (!response.ok) {
+      throw response.json();
+    }
+    return response.json();
+    })
   
-        .then(function (forecastData) {
-    
-          if (!forecastData) {
-            alert(`No weather forecast data found!`);
-          } else {
-            forecastContainer.innerHTML = '';
+  .then(function (forecastData) {
+    if (!forecastData) {
+      alert(`No weather forecast data found!`);
+    } else {
+      forecastContainer.innerHTML = '';
             
-            let index = [7, 15, 23, 31, 39]
-            for (let i = 0; i < index.length; i++) {
-              let forecastIndex = index[i];
-              displayForecast(forecastData.list[forecastIndex]);
-            }
-          }
-        })
+      let index = [7, 15, 23, 31, 39]
+      for (let i = 0; i < index.length; i++) {
+        let forecastIndex = index[i];
+        displayForecast(forecastData.list[forecastIndex]);
+      }
+
+      forecastHeader.classList.remove('hidden');
+    }
+  })
         
-        .catch(function (error) {
-          console.error(error);
-        });
+  .catch(function (error) {
+    console.error(error);
+  });
 }
 
 function displayWeather(data) {
-    const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    currentCity.innerHTML = `${data.name} (${new Date(data.dt * 1000).toLocaleDateString()}) <img src="${iconURL}" alt="Weather icon">`;
-    currentTemp.textContent = `Temp: ${data.main.temp} °F`;
-    currentWind.textContent = `Wind: ${data.wind.speed} MPH`;
-    currentHumidity.textContent = `Humidity: ${data.main.humidity} %`;
+  const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  currentCity.innerHTML = `${data.name} (${new Date(data.dt * 1000).toLocaleDateString()}) <img src="${iconURL}" alt="Weather icon">`;
+  currentTemp.textContent = `Temp: ${data.main.temp} °F`;
+  currentWind.textContent = `Wind: ${data.wind.speed} MPH`;
+  currentHumidity.textContent = `Humidity: ${data.main.humidity} %`;
+
+  currentWeatherEl.classList.remove('hidden');
 }
 
 function displayForecast(forecast) {
@@ -131,7 +133,7 @@ function renderSearchHistory() {
     historyButton.textContent = city;
     historyButton.classList.add('history-button');
     historyButton.addEventListener('click', () => {
-        fetchWeather(city);
+      fetchWeather(city);
     });
     searchHistoryEl.appendChild(historyButton);
   });
